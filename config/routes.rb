81964dd9e13b67ca0cece6e2dcd7354bfb779378
Eduard Kunce <eduard.kunce@gmail.com>
2013-09-04 10:27:54 +0200

    C1 iteration - store, routing, style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
+
+  root to: 'store#index', as: 'store'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -47,10 +51,6 @@ Depot::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
