41dbf10833f30e68158f744ddbb2953a72c23e28
Eduard Kunce <eduard.kunce@gmail.com>
2013-09-05 14:38:27 +0200

    working ajax version

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -84,24 +84,25 @@ class LineItemsController < ApplicationController
       format.json { head :no_content }
     end
   end
-end
 
-# POST /line_items/1
-# POST /line_items/1.json
-def decrement
-  @cart = current_cart
+  # POST /line_items/1
+  # POST /line_items/1.json
+  def decrement
+    @cart = current_cart
 
-  @line_item = @cart.decrement_line_item_quantity(params[:id]) # passing in line_item.id
+    @line_item = @cart.decrement_line_item_quantity(params[:id]) # passing in line_item.id
 
-  respond_to do |format|
-    if @line_item.save
-      format.html { redirect_to store_path, notice: 'Line item was successfully updated.' }
-      format.js {@current_item = @line_item}
-      format.json { head :ok }
-    else
-      format.html { render action: "edit" }
-      format.js {@current_item = @line_item}
-      format.json { render json: @line_item.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_path, notice: 'Line item was successfully updated.' }
+        format.js {@current_item = @line_item}
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.js {@current_item = @line_item}
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
     end
   end
 end
+
